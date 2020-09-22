f31957a74d4b8504e5ad6953b0786a2110786d31
sujl95 <sujl95@naver.com>
2020-09-23 02:59:55 +0900

    InterfaceAbstract,enum

---
+++ src/kr/javaPractice/GodOfJava/_13InterfaceAndAbstractAndEnum/enums/OverTimeManager3.java
@@ -0,0 +1,11 @@
+package kr.javaPractice.GodOfJava._13InterfaceAndAbstractAndEnum.enums;
+
+public class OverTimeManager3 {
+    public static void main(String[] args) {
+        OverTimeValues2[] valueList = OverTimeValues2.values();
+
+        for(OverTimeValues2 value : valueList){
+            System.out.println(value);
+        }
+    }
+}
