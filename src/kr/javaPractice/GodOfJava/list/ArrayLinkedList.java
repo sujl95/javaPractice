67f03d5dcc9be5e5dc894a0339754873025f7059
sujl95 <sujl95@naver.com>
2020-10-05 00:01:13 +0900

    ArrayLinkedList

--- src/kr/javaPractice/GodOfJava/list/ArrayLinkedList.java
+++ src/kr/javaPractice/GodOfJava/list/ArrayLinkedList.java
@@ -8,27 +8,19 @@ public class ArrayLinkedList {
     public static void main(String[] args) {
         ArrayList<Object> arrayList = new ArrayList<>();
         LinkedList<Object> linkedList = new LinkedList<>();
-
         linkedList.add("1");
         linkedList.add("2");
         linkedList.add("3");
         arrayList.add("1");
         arrayList.add("2");
         arrayList.add("3");
-
         System.out.println(arrayList);
-
         linkedList.remove(1);
         linkedList.add("2");
-
         System.out.println(linkedList);
         System.out.println(linkedList.get(0));
         System.out.println("ck");
         String[] strlist1 = arrayList.toArray(new String[10]);
-
         System.out.println(Arrays.toString(strlist1));
-
-
-
     }
 }
