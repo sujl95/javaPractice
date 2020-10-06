1552ed8e22490fbd20497fde4adb05d12fe82488
sujl95 <sujl95@naver.com>
2020-10-07 00:06:03 +0900

    Serialize

---
+++ src/kr/javaPractice/GodOfJava/JVM/ClassLoaderTest.java
@@ -0,0 +1,12 @@
+package kr.javaPractice.GodOfJava.JVM;
+
+public class ClassLoaderTest extends JavaClassLoader {
+
+    public static void main(String[] args) {
+
+        JavaClassLoader javaClassLoader = new JavaClassLoader();
+        javaClassLoader.invokeClassMethod("kr.javaPractice.GodOfJava.JVM.MyClass", "sayHello");
+
+    }
+
+}
---
+++ src/kr/javaPractice/GodOfJava/_15String/Test.java
@@ -0,0 +1,7 @@
+package kr.javaPractice.GodOfJava._15String;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = new String("test");
+    }
+}
