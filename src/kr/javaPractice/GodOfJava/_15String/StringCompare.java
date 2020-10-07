6cdfd3b08415446184d3be6a3ba8e5779659b4a6
sujl95 <sujl95@naver.com>
2020-10-08 01:51:37 +0900

    StringCompare

--- src/kr/javaPractice/GodOfJava/_15String/StringCompare.java
+++ src/kr/javaPractice/GodOfJava/_15String/StringCompare.java
@@ -3,7 +3,12 @@ package kr.javaPractice.GodOfJava._15String;
 public class StringCompare {
     public static void main(String[] args) {
         StringCompare sample = new StringCompare();
-        sample.checkCompareTo();
+
+        String[] addresses = new String[]{
+          "서울시 구로구 신림동","경기도 성남시 분당구 정자동 개발 공장",
+            "서울시 구로구 개봉동",
+        };
+        sample.checkMatch();
     }
 
     private void checkCompare() {
@@ -26,6 +31,44 @@ public class StringCompare {
         System.out.println(text2.compareTo(text));
         System.out.println(text2.compareTo(text3));
         System.out.println(text.compareTo(text3));
+    }
+
+    public void checkAddress(String[] addresses) {
+        int startCount = 0, endCount = 0;
+        String startText = "서울시";
+        String endText = "동";
+        for(String address: addresses){
+            if (address.startsWith(startText)){
+                startCount++;
+            }
+            if (address.endsWith(endText)){
+                endCount++;
+            }
+        }
+        System.out.println("Starts with " + startText + " count is " + startCount);
+        System.out.println("Ends with " + endText + " count is " + endCount);
+    }
+
+    public void containsAddress(String[] addresses){
+        int containCount=0;
+        String containText = "구로";
+        for (String address : addresses) {
+            if(address.contains(containText)) {
+                containCount++;
+            }
+        }
+
+        System.out.println("Contains " + containText + " count is " + containCount);
+
+    }
+
+    public void checkMatch() {
+        String text = "This is a text";
+        String compare1 = "is";
+        String compare2 = "this";
+        System.out.println(text.regionMatches(2, compare1, 0, 1));
+        System.out.println(text.regionMatches(5, compare1, 0, 2));
+        System.out.println(text.regionMatches(true,0, compare2, 0, 4));
     }
 
 }
