6b5efca9ffccecb1ffff4bd64d08c592e667a4d9
sujl95 <sujl95@naver.com>
2020-10-11 02:12:57 +0900

    add

--- src/kr/javaPractice/GodOfJava/JVM/ClassLoaderTest.java
+++ src/kr/javaPractice/GodOfJava/JVM/ClassLoaderTest.java
@@ -3,10 +3,7 @@ package kr.javaPractice.GodOfJava.JVM;
 public class ClassLoaderTest extends JavaClassLoader {
 
     public static void main(String[] args) {
-
         JavaClassLoader javaClassLoader = new JavaClassLoader();
         javaClassLoader.invokeClassMethod("kr.javaPractice.GodOfJava.JVM.MyClass", "sayHello");
-
     }
-
 }
