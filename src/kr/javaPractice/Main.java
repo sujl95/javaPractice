3007a00728aee2e1f495fee1bbe76677c5bca850
sujl95 <sujl95@naver.com>
2020-10-12 00:35:47 +0900

    add

--- src/kr/javaPractice/Main.java
+++ src/kr/javaPractice/Main.java
@@ -3,13 +3,5 @@ package kr.javaPractice;
 public class Main {
 
     public static void main(String[] args) {
-        int i1 = 4000000;
-        int i2 = 122;
-        int i3 = 3;
-
-        String str = new String("test");
-        str = "1234";
-
-        System.out.println(str);
     }
 }
