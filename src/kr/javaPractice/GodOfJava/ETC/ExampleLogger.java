38b8f019d3bbe46e5d720367a7975d5896dd3d15
sujl95 <sujl95@naver.com>
2020-10-16 00:04:09 +0900

    ExampleLogger

--- src/kr/javaPractice/GodOfJava/ETC/ExampleLogger.java
+++ src/kr/javaPractice/GodOfJava/ETC/ExampleLogger.java
@@ -2,16 +2,23 @@ package kr.javaPractice.GodOfJava.ETC;
 
 import kr.javaPractice.Main;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ExampleLogger {
     private static Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
-
+        List<String> list = new ArrayList<String>();
+        list.add("123");
+        list.add("123");
+        System.out.println(list);
 
         int[] arr = new int[5];
         try{
+            System.out.println(arr);
             System.out.println(arr[5]);
         } catch (Exception e) {
             logger.log(Level.SEVERE,"SEVERE, {0}" ,e.toString());
