a77bdfba0c2f15b39792bf26ff5757b54a00e359
sujl95 <sujl95@naver.com>
2020-10-23 00:00:18 +0900

    sample

--- src/kr/javaPractice/InterfaceExample.java
+++ src/kr/javaPractice/InterfaceExample.java
@@ -4,6 +4,7 @@ import kr.javaPractice.GodOfJava._13InterfaceAndAbstractAndEnum.MemberManager;
 import kr.javaPractice.GodOfJava._13InterfaceAndAbstractAndEnum.MemberManagerImpl;
 
 public class InterfaceExample {
+
     public static void main(String[] args) {
         MemberManager member = new MemberManagerImpl();
     }
