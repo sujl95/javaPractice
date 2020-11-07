47341e70e7ba73439cc7ab6c9f4fb844c4ed7128
sujl95 <sujl95@naver.com>
2020-11-08 00:13:22 +0900

    Integer Test

--- src/kr/javaPractice/Main.java
+++ src/kr/javaPractice/Main.java
@@ -1,8 +1,6 @@
 package kr.javaPractice;
 
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 import java.util.*;
 
 public class Main {
@@ -11,5 +9,20 @@ public class Main {
         Stack<String> stack = new Stack<>();
         stack.push("1");
         stack.get(0);
+        Integer a = 1;
+        Integer a1 = new Integer(2)+new Integer(3);
+        System.out.println(a1);
+        Integer a2 = 3;
+
+        Integer a3 = a+a1;
+
+        System.out.println(a2.equals(a3));
+
+
+        System.out.println(a.hashCode());
+        System.out.println(a1.hashCode());
+        System.out.println(a2.hashCode());
+        System.out.println(a.equals(a1));
+        System.out.println(a*a1*a2);
     }
 }
