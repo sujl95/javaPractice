3ec27f9b82f1b7b77d2d5895884d3eacf5197134
sujl95 <sujl95@naver.com>
2020-12-03 00:59:07 +0900

    스프링을 통한 의존성 주입 - @Autowired vs @Resource vs <property> 태그 사례 연구 6. XML 설정 - 두 개의 빈이 tire 인터페이스를 구현하고 속성과 일치하는 id가 없지만 @Resource 어노테이션의 name 속성이 id와 일치하는 경우 사례 연구 7. 사례 연구 6과 같도록 @Autowired를 지정하려면 다음과 같이 설정 사례 연구 8. 실무라면 필자는 다음과 같이 설정하겠다.

--- ExpertSpring30/src/main/java/me/thewing/expert006/Car.java
+++ ExpertSpring30/src/main/java/me/thewing/expert006/Car.java
@@ -1,12 +1,13 @@
 package me.thewing.expert006;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 
 import javax.annotation.Resource;
 
 public class Car {
-    @Resource
+    @Resource(name = "tire")
     Tire tire;
 
     public String getTireBrand() {
