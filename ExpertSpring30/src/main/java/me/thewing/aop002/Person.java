7b3b78f74c15f5ecedf06db9e311aa28d7c5fe9e
sujl95 <sujl95@naver.com>
2020-12-25 03:12:06 +0900

    add DequeSample Person

---
+++ ExpertSpring30/src/main/java/me/thewing/aop002/Person.java
@@ -0,0 +1,5 @@
+package me.thewing.aop002;
+
+public interface Person {
+    void runSomething();
+}
---
+++ javaPractice/src/kr/javaPractice/GodOfJava/ETC/DequeSample.java
@@ -0,0 +1,11 @@
+package kr.javaPractice.GodOfJava.ETC;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class DequeSample {
+    public static void main(String[] args) {
+        Deque deque = new ArrayDeque();
+        
+    }
+}
