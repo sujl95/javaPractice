1ccc4f5cae913e5d97e062300d3a19d9968e6148
sujl95 <sujl95@naver.com>
2021-01-02 02:36:29 +0900

    주소API 파일 생성

---
+++ javaPractice/src/kr/javaPractice/GodOfJava/_27SerializableAndNIO/CsvSample.java
@@ -0,0 +1,114 @@
+package kr.javaPractice.GodOfJava._27SerializableAndNIO;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvSample {
+	private static List<String> list = new ArrayList<>();
+
+
+	public static void main(String[] args) throws Exception {
+		String sfileName = "C:\\godofjava\\test.csv";
+		Path fileName = Paths.get("C:\\godofjava\\test.csv");
+		Path fileName2 = Paths.get("C:\\godofjava\\test2.csv");
+		Path fileName3 = Paths.get("C:\\godofjava\\test3.csv");
+		readWriteFile(fileName, fileName3);
+		// readFile(fileName);
+		// writeFile(fileName3);
+		// readEncode(fileName);
+		// readUnicodeJava11(sfileName);
+	}
+
+	private static void readWriteFile(Path fileName, Path fileName2) throws IOException{
+		try (BufferedReader br = Files.newBufferedReader(fileName, StandardCharsets.UTF_8);
+			BufferedWriter bw = Files.newBufferedWriter(fileName2, StandardCharsets.UTF_8)){
+			String line = "";
+			while((line = br.readLine()) != null) {
+				line = line.replace(';',',');
+				line = line.replace("특별자치도", "");
+				line = line.replace("청", "");
+				line = line.replace("라", "");
+				line = line.replace("상", "");
+				line = line.replace("도", "");
+				line = line.replace("특별시", "");
+				line = line.replace("광역시", "");
+				bw.append(line);
+				bw.newLine();
+			}
+		}
+	}
+
+	private static void writeFile(Path fileName) throws IOException {
+		try (BufferedWriter writer = Files.newBufferedWriter(fileName, StandardCharsets.UTF_8);) {
+			for (String line : list) {
+				writer.append(line);
+				writer.newLine();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void readFile(Path fileName) throws Exception {
+		try (BufferedReader br = Files.newBufferedReader(fileName, StandardCharsets.UTF_8);){
+			String line = "";
+			while((line = br.readLine()) != null) {
+				line = line.replace(';',',');
+				line = line.replace("특별자치도", "");
+				line = line.replace("청", "");
+				line = line.replace("라", "");
+				line = line.replace("상", "");
+				line = line.replace("도", "");
+				line = line.replace("특별시", "");
+				line = line.replace("광역시", "");
+				System.out.println(line);
+
+				list.add(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	// Java 11, adds charset to FileReader
+	public static void readUnicodeJava11(String fileName) {
+		try (FileReader fr = new FileReader(fileName, StandardCharsets.UTF_8);
+			 BufferedReader reader = new BufferedReader(fr)) {
+
+			String str;
+			while ((str = reader.readLine()) != null) {
+				str = str.replace(';',',');
+				str = str.replace("특별자치도", "");
+				str = str.replace("청", "");
+				str = str.replace("라", "");
+				str = str.replace("상", "");
+				str = str.replace("도", "");
+				str = str.replace("특별시", "");
+				str = str.replace("광역시", "");
+				System.out.println(str);
+
+				list.add(str);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
