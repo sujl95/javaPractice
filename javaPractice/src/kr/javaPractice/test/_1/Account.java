080480975c5da727e957896c555e070d319312f6
sujl95 <sujl95@naver.com>
2021-01-06 15:27:01 +0900

    if TDD

---
+++ javaPractice/src/kr/javaPractice/test/_1/Account.java
@@ -0,0 +1,13 @@
+package kr.javaPractice.test._1;
+
+public class Account {
+	private int balance;
+
+	public Account (int money ) {
+		this.balance = money;
+	}
+
+	public int getBalance() {
+		return this.balance;
+	}
+}
