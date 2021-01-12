bb31683e00ef661de46f6d79afae2e9e9a4fc281
sujl95 <sujl95@naver.com>
2021-01-13 00:03:44 +0900

    Conditional Add

--- ConditionPractice/src/main/java/me/thewing/conditiontest/ConditionalOnBean/Parent.java
+++ ConditionPractice/src/main/java/me/thewing/conditiontest/ConditionalOnBean/Parent.java
@@ -1,7 +1,15 @@
 package me.thewing.conditiontest.ConditionalOnBean;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 
-@ConditionalOnClass
+import me.thewing.conditiontest.ConditionTest2.Client;
+
+@ConditionalOnClass(Client.class)
+// @ConditionalOnBean
+// @ConditionalOnMissingBean
 public class Parent {
 }
+
+
